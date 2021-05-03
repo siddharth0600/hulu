ac4b21c2d9a2f4945eb577ef217b0f149a7cec99
siddharth0600 <siddharthtripathi00@gmail.com>
2021-05-03 22:12:12 +0530

    Result and thumbnail component complete

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export async function getServerSideProps(context) {
 
   const request = await fetch(
     `https://api.themoviedb.org/3${
-      requests[genre]?.url || requests.fetchTrending.url
+      requests[genre]?.url || requests.fetchTopRated.url
     }`
   ).then((res) => res.json());
 
--- utils/requests.js
+++ utils/requests.js
@@ -1,10 +1,10 @@
 const API_KEY = process.env.API_KEY;
 
 export default {
-  fetchTrending: {
-    title: "Trending",
-    url: `/trending/all/week/?api_key=${API_KEY}&language=en-US`,
-  },
+  // fetchTrending: {
+  //   title: "Trending",
+  //   url: `/trending/all/week/?api_key=${API_KEY}&language=en-US`,
+  // },
   fetchTopRated: {
     title: "Top Rated",
     url: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
