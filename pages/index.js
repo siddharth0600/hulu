c7108cf61480abec6bfea82fa1d63d23b21852f5
siddharth0600 <siddharthtripathi00@gmail.com>
2021-05-03 17:20:09 +0530

    result component complete

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from "next/head";
-import Image from "next/image";
+
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import Results from "../components/Results";
+import requests from "../utils/requests";
 
-export default function Home() {
+export default function Home({ results }) {
+  // console.log(props);
   return (
     <div>
       <Head>
@@ -19,6 +22,24 @@ export default function Home() {
       <Nav />
 
       {/* Results */}
+      <Results results={results} />
     </div>
   );
+}
+
+// Below code will run on the server side first then props will be passed to home (see above code)
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
